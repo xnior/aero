42a6e4e3c83ac7873ce289d6888215738c533345
Adhemar Jr <adhemar.machado@gmail.com>
2025-04-25 20:03:01 -0300

    Updates

---
+++ assets/DB/process.db.ts
@@ -0,0 +1,12 @@
+import {
+  AErodromesPrivAnac,
+  Updated
+} from "../../app/interface/aerodromes-priv-anac.interface";
+import { databasePublic } from "./publ.db";
+import { databasePriv } from "./priv.db";
+
+
+export const UpdatedDate: Updated = { updated: "15/04/2025" };
+export const AERODROMES_ANAC_PRIVATE: AErodromesPrivAnac[] = [ ...databasePriv, ...databasePublic ]
+
+
